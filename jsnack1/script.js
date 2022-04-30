c82146186e54448a30cdc78def59a4f7080b87c5
RiccardoBeca <riccardobeca@gmail.com>
2022-04-30 13:52:31 +0100

    init

---
+++ jsnack1/script.js
@@ -0,0 +1,62 @@
+/* 
+
+Crea un array di 10 oggetti che rappresentano una zucchina, indicando per ognuna varietà, peso e lunghezza.
+Calcola quanto pesano tutte le zucchine.
+
+
+
+*/
+
+
+const courgetteVariety = [
+ {
+   nome: "zucchino nero di Milano",
+   peso: 10,
+   lunghezza: 14
+ },
+ {
+  nome: "zucchino romanesco",
+  peso: 10,
+  lunghezza: 14
+},
+{
+  nome: "zucchino ortolano di Faenza",
+  peso: 10,
+  lunghezza: 14
+},
+{
+  nome: "zucchina lunga fiorentina",
+  peso: 10,
+  lunghezza: 14
+},
+{
+  nome: "zucchino siciliano",
+  peso: 10,
+  lunghezza: 14
+},
+{
+  nome: "zucchina striata di Napoli",
+  peso: 10,
+  lunghezza: 14
+},
+{
+  nome: "zucchina bianca triestina",
+  peso: 10,
+  lunghezza: 14
+},
+{
+  nome: "zucchina rigata pugliese",
+  peso: 10,
+  lunghezza: 14
+},
+{
+  nome: "zucchino tondo di Piacenza",
+  peso: 10,
+  lunghezza: 14
+},
+{
+  nome: "zucchino tondo di Nizza",
+  peso: 10,
+  lunghezza: 14
+},
+]
